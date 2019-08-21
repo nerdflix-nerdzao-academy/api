a9740df7ba5da2ce37afc18018bf0d834f8a73c7
demagsi <denis.magalhaes-silva@itau-unibanco.com.br>
2019-08-21 10:55:22 -0300

    Seeds of database

--- database/seeds/DatabaseSeeder.js
+++ database/seeds/DatabaseSeeder.js
@@ -1,20 +1,77 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| DatabaseSeeder
-|--------------------------------------------------------------------------
-|
-| Make use of the Factory instance to seed database with dummy data or
-| make use of Lucid models directly.
-|
-*/
-
-/** @type {import('@adonisjs/lucid/src/Factory')} */
-const Factory = use('Factory')
+const Database = use('Database')
+const Products = use('App/Models/Products')
+const ProductsTypes = use('App/Models/ProductsTypes')
+const Status = use('App/Models/Status')
+const { status } = use('./../../config/enums');
 
 class DatabaseSeeder {
-  async run () {
+  async run() {
+    await Database.raw('PRAGMA foreign_keys = OFF;');
+    await Status.truncate();
+    await Products.truncate();
+    await ProductsTypes.truncate();
+
+    const statusData = Object.keys(status).map(index => status[index]);
+    await Status.createMany(statusData);
+
+    await ProductsTypes.createMany([
+      {
+        id: 1,
+        text: 'Alimentação',
+        status_id: 1,
+      },
+      {
+        id: 2,
+        text: 'Bebida',
+        status_id: 1,
+      }
+    ]);
+
+    await Products.createMany([
+      {
+        id: '1fffe522-e46f-4366-9cb9-bb4807b58b59',
+        status_id: 1,
+        name: "Arroz",
+        description: "Lorem ipsum",
+        product_type_id: 1,
+        data: {
+          payment: [1, 2, 3],
+        }
+
+      },
+      {
+        id: 'dae2d1a4-b00b-4281-980e-875f31e188e1',
+        status_id: 1,
+        name: "Feijão",
+        description: "Lorem ipsum",
+        product_type_id: 1,
+
+      },
+      {
+        id: '62e00834-a0ac-42fd-88f7-9a781b95ec0c',
+        status_id: 1,
+        name: "Coca cola",
+        description: "Lorem ipsum",
+        product_type_id: 2,
+        data: {
+          payment: [1, 2, 3],
+        }
+      },
+      {
+        id: 'aed62762-8e08-49ab-b60f-9c10d9ec9cc9',
+        status_id: 1,
+        name: "Fanta",
+        description: "Lorem ipsum",
+        product_type_id: 2,
+        data: {
+          payment: [1, 2, 3],
+        }
+      },
+    ]);
+
+    await Database.raw('PRAGMA foreign_keys = ON;')
   }
 }
 
